f0e675b051d716c7947a21569fa3336f38d0b12d
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-12-07 20:06:01 +0100

    Update default version

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,10 +4,10 @@
 #
 #   include jenkins_for_mac
 #   class { 'jenkins_for_mac':
-#     version => '1.532'
+#     version => '1.542'
 #   }
 #
-class jenkins_for_mac($version='1.532') {
+class jenkins_for_mac($version='1.542') {
 
   package { "jenkins-${version}.pkg":
     provider => 'pkgdmg',
