21a3f3950b575a555cf30018d65c025a758d4207
Dieter De Meyer <dieterdemeyer@gmail.com>
2014-03-02 15:31:38 +0100

    Update to latest version

--- spec/classes/jenkins_for_mac_spec.rb
+++ spec/classes/jenkins_for_mac_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'jenkins_for_mac' do
 
-  version='1.532'
+  version='1.552'
 
   it { should contain_class('jenkins_for_mac') }
   it { should contain_package("jenkins-#{version}.pkg").with_provider('pkgdmg') }
